641b2de263ab115ac10792297d0427707b0b116a
Jhang <zcr19930225@live.com>
2016-08-30 22:13:38 +0800

    exercise 11-13

---
+++ ex13.1.py
@@ -0,0 +1,6 @@
+from sys import argv
+a, b, c = argv
+
+print "The script name is", a
+print "YanYan's birthday is", b
+print "Our anniversaryday is", c
---
+++ ex14.py
@@ -0,0 +1,6 @@
+from sys import agrv
+a, b, c = argv
+
+print "The script name is", a
+print "YanYan's birthday is", b
+print "Our anniversaryday is", c 
